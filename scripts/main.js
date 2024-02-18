326249026e93d7b80e9ab802c953583d2f0d0dde
nazmul37290 <nazmulnjm@gmail.com>
2024-02-18 18:23:20 -0400

    discount added

---
+++ scripts/main.js
@@ -0,0 +1,106 @@
+const seats = document.getElementsByClassName("seat");
+let ticketCount = 0;
+
+for (const seat of seats) {
+  seat.addEventListener("click", function (event) {
+    ticketCount++;
+    if (ticketCount === 4) {
+      const couponBtn = document.getElementById("coupon-btn");
+      couponBtn.removeAttribute("disabled");
+    }
+    if (ticketCount > 4) {
+      alert("you cant buy more than 4");
+      return;
+    }
+
+    const tr = document.createElement("tr");
+    const td1 = document.createElement("td");
+    td1.innerText = seat.innerText;
+    const td2 = document.createElement("td");
+    td2.innerText = "Economy";
+
+    const td3 = document.createElement("td");
+    td3.innerText = 550;
+
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    console.log(tr);
+    const ticketListContainer = document.querySelector("table");
+    ticketListContainer.appendChild(tr);
+
+    const totalPrice = document.getElementById("total-price");
+    const calculatedTotalPrice = parseInt(totalPrice.innerText) + 550;
+    totalPrice.innerText = calculatedTotalPrice;
+
+    const grandTotal = getInnerText("grand-total");
+    setInnerText("grand-total", calculatedTotalPrice);
+    document
+      .getElementById("coupon-btn")
+      .addEventListener("click", function () {
+        const couponText = getInputValue("coupon-text");
+        if (couponText === "NEW15") {
+          const discount = (calculatedTotalPrice * 15) / 100;
+          calculatedGrandTotal = calculatedTotalPrice - discount;
+          setInnerText("grand-total", calculatedGrandTotal);
+          //   _________
+          const child = document.getElementById("container");
+          const div = document.createElement("div");
+          const p1 = document.createElement("div");
+          p1.innerText = "Discount";
+          const p2 = document.createElement("div");
+          p2.innerText = discount;
+
+          div.appendChild(p1);
+          div.appendChild(p2);
+          div.classList.add("flex", "justify-around");
+
+          child.appendChild(div);
+          console.log(child.parentNode);
+        } else if (couponText === "Couple 20") {
+          const discount = (calculatedTotalPrice * 20) / 100;
+          calculatedGrandTotal = calculatedTotalPrice - discount;
+          setInnerText("grand-total", calculatedGrandTotal);
+        } else {
+          grandTotal = calculatedTotalPrice;
+        }
+      });
+
+    // ___________
+    setInnerText("selected-seat-count", ticketCount);
+
+    const ticketLeft = document.getElementById("ticket-left");
+    ticketLeft.innerText = ticketLeft.innerText - 1;
+    seat.classList.add("bg-primary", "text-white");
+  });
+}
+
+function setInnerText(elemenId, value) {
+  const element = document.getElementById(elemenId);
+  element.innerText = value;
+}
+function getInnerText(elemenId) {
+  const element = document.getElementById(elemenId);
+  const text = element.innerText;
+  return text;
+}
+function getInputValue(elemenId) {
+  const element = document.getElementById(elemenId);
+  const text = element.value;
+  return text;
+}
+
+function addDiscountLine(elemenId, value) {
+  const element = document.getElementById(elemenId);
+  const div = document.createElement("div");
+  const p1 = document.createElement("div");
+  p1.innerText = "Discount";
+  const p2 = document.createElement("div");
+  p2.innerText = value;
+
+  div.appendChild(p1);
+  div.appendChild(p2);
+  div.classList.add("flex", "justify-around");
+
+  element.appendChild(div);
+}
